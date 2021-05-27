08105a1ff120320ddff35fa850518f003a67b726
Aurelius Prochazka <aurephd@gmail.com>
2021-05-27 11:39:42 -0700

    Cleanup Package.swift

--- Package.swift
+++ Package.swift
@@ -5,27 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "MicrotonalAudioKit",
-    platforms: [
-        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "MicrotonalAudioKit",
-            targets: ["MicrotonalAudioKit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/AudioKit/AudioKit", .branch("develop")),
-    ],
+    platforms: [.macOS(.v10_14), .iOS(.v13), .tvOS(.v13)],
+    products: [.library(name: "MicrotonalAudioKit", targets: ["MicrotonalAudioKit"])],
+    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", .branch("develop"))],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MicrotonalAudioKit",
-            dependencies: ["AudioKit"]),
-        .testTarget(
-            name: "MicrotonalAudioKitTests",
-            dependencies: ["MicrotonalAudioKit"]),
+        .target(name: "MicrotonalAudioKit", dependencies: ["AudioKit"]),
+        .testTarget(name: "MicrotonalAudioKitTests", dependencies: ["MicrotonalAudioKit"])
     ]
 )
