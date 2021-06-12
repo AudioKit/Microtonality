10be84d31188af72444c4a59d95dc7e30360de59
Aurelius Prochazka <aurephd@gmail.com>
2021-06-12 13:22:32 -0700

    Reducing minimum deployment

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MicrotonalAudioKit",
-    platforms: [.macOS(.v10_14), .iOS(.v13), .tvOS(.v13)],
+    platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
     products: [.library(name: "MicrotonalAudioKit", targets: ["MicrotonalAudioKit"])],
-    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", from: "5.2.0")],
+    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", .branch("develop"))],
     targets: [
         .target(name: "MicrotonalAudioKit", dependencies: ["AudioKit"]),
         .testTarget(name: "MicrotonalAudioKitTests", dependencies: ["MicrotonalAudioKit"])
