5aa7435bce8dd634d41633404c83b2ae6cb150da
Aurelius Prochazka <aurephd@gmail.com>
2022-01-18 00:43:38 -0800

    Updated to AK 5.3

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "MicrotonalAudioKit",
     platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
     products: [.library(name: "MicrotonalAudioKit", targets: ["MicrotonalAudioKit"])],
-    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", from: "5.2.0")],
+    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", from: "5.3.0")],
     targets: [
         .target(name: "MicrotonalAudioKit", dependencies: ["AudioKit"]),
         .testTarget(name: "MicrotonalAudioKitTests", dependencies: ["MicrotonalAudioKit"])
